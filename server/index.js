e4e5ac5d7cc24aadee101d190e7ec018923409cc
burjoy <luqman.hadi85@gmail.com>
2025-05-26 07:36:14 +0700

    added placeholder for server use

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
