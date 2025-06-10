65191c77b4694d6f41c28b684e456502718214c7
burjoy <luqman.hadi85@gmail.com>
2025-06-10 08:53:45 +0700

    added groundwork for formsController function

--- server/controller/FormController.js
+++ server/controller/FormController.js
@@ -1,13 +1,22 @@
+
 const formsDB = {
-    forms: require('../../model/model_tamu.json'),
-    forms_action: function (data) { this.forms = data }
+    forms_action: function (data) { this.forms = data },
+    forms: require('../../model/model_tamu.json')
 }
 
 const FormController = async (req, res) => {
     try {
-        // Simulate a delay to mimic database access
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
+        const { instansi, email, pesan, waktu } = req.body;
+        // Validate required fields
+        if (!instansi || !email || !pesan || !waktu) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const foundInstansi = formsDB.forms.find(form => form.instansi === instansi);
+        if(!foundInstansi){
+            const newData = { instansi, email, pesan, waktu };
+            formsDB.forms_action([...formsDB.forms, newData]);
+        }
+        console.log(foundInstansi);
         // Return the forms data
         res.status(200).json(formsDB.forms);
     } catch (error) {
@@ -15,3 +24,5 @@ const FormController = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 }
+
+module.exports = { FormController };
